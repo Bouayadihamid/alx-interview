ee3b78f94fb6428685a678ea27e5c9861d40797e
Hamid <Bouayadihamid@gmail.com>
2024-06-28 08:20:34 -0700

    Use tab

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,14 +9,14 @@ def pascal_triangle(n):
     representing the Pascal Triangle of n
     returns empty list if n <= 0
     """
+    tab = []
     if n <= 0:
-        return []
+        return tab
 
-    triangle = []
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        triangle.append(row)
-    return triangle
+            row[j] = tab[i-1][j-1] + tab[i-1][j]
+        tab.append(row)
+    return tab
 
